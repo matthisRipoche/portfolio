8f89c434ac06f28b761e4c24312dfe5ee7f0c304
matthisRipoche <matthisripoche@gmail.com>
2025-02-27 10:25:44 +0100

    fix2

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Matthis Ripoche - Portfolio</title>
+    <title>Matthisssss Ripoche - Portfolio</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
