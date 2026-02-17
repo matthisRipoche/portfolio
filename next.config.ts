175bf3b940660fb308dd1d99aaa158f77a3c08bf
matthisRipoche <matthisripoche@gmail.com>
2026-02-17 18:51:05 +0100

    dockeriser

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Active le mode standalone pour Docker */
+  output: "standalone",
+
+  /* Tes autres options ici */
 };
 
 export default nextConfig;
